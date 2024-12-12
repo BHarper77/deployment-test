11155ba6d0861051bb6c87a6fd212608ad2945fe
Brady Harper <58574645+BHarper77@users.noreply.github.com>
2024-12-12 14:38:28 +0000

    Improved logging (#9)

--- index.ts
+++ index.ts
@@ -10,10 +10,12 @@ try {
 
 /** Logs errors */
 function logError(message: string) {
+	console.error(message)
 	throw new Error(message)
 }
 
 /** Logs messages */
 function logMessage(message: string) {
+	console.log("Message from prccess")
 	console.log(message)
 }
